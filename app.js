e3f113779e0bd39c3a36928662bea6271b38df80
Jorden Tang <jordentangwork@gmail.com>
2019-12-29 22:50:41 -0800

    completed ajax request job details, highlight job selection, highlight selected page tab

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user:'', //db user name
-    password:'', //db password
+    user:'fruitboy', //db user name
+    password:'Aa745892475', //db password
     database:'posting_web'
 });
 
@@ -25,10 +25,41 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get('/', (request, response) => {
-    post_array = [];
+    response.redirect("/jobpost/1")
+});
+
+app.get("/job/:jobId", function (req, res) {
+    connection.query("SELECT * FROM company_posts WHERE Company_id = ' " + req.params.jobId + "'", (err, rows, fields)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(rows[0]);
+        }
+    })
+})
+
+app.get('/jobpost/:pageIndex', (request, response) => {
     connection.query('SELECT * FROM company_posts', (err, rows, fields)=>{
         if(!err){
-            response.render('index', {all_posts: rows});
+            var pageIndex = parseInt(request.params.pageIndex);
+            var total_page_length = Math.ceil(rows.length / 10);
+            var pagin_per_page = 5;
+            var start = (pageIndex - 1) * pagin_per_page;
+            var end = start + 10;
+            var pagin_start = pageIndex - 2;
+            var pagin_end = pageIndex + 3;
+
+            //edge cases for first few pages and last few pages
+            if(pagin_start < 1){
+                pagin_start = 1;
+                pagin_end = pagin_start + 5;
+            }
+            if(pagin_end > total_page_length){
+                pagin_end = total_page_length;
+                pagin_start = pagin_end - 5;
+            }
+            response.render('index', {all_posts: rows, start: start, end: end, pagin_start: pagin_start, pagin_end: pagin_end, last_page: total_page_length - 1, currPage: pageIndex});
         }
         else{
             console.log(err);
