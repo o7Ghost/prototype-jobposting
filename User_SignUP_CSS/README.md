dce3e3144abb5791135b9fd4a62401f929e0f357
Joseph <qjlu@ucsd.edu>
2020-01-02 14:42:56 -0800

    master

---
+++ User_SignUP_CSS/README.md
@@ -0,0 +1,6 @@
+style/CSS all right reserved to
+Author: Colorlib
+Author URL: https://colorlib.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+
---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+var mysql = require('mysql');
+app.use(express.static(__dirname + "/User_SignUP_CSS"));
+app.use('/Login', require('./signIn.js'));
+app.use('/SignUp', require('./register.js'));
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '',     // your root username
+  password : '',
+  database : 'Posting_Web',   // the name of your db
+  multipleStatements: true
+});
+
+connection.connect(function(error){
+    if(error){
+        console.log(error);
+        console.log('database errors');
+    }
+    else{
+        console.log('connected');
+    }
+});
+
+//Tells the server to look for static files from a folder called "static" from root
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (request, response) => {
+    response.redirect("/jobpost/1")
+});
+
+app.get("/job/:jobId", function (req, res) {
+    connection.query("SELECT * FROM company_posts WHERE Company_id = ' " + req.params.jobId + "'", (err, rows, fields)=>{
+        if(err){
+            console.log(err);
+        }
+        //sending json 
+        else{
+            res.send(rows[0]);
+        }
+    })
+})
+
+app.get('/jobpost/:pageIndex', (request, response) => {
+    connection.query('SELECT * FROM company_posts', (err, rows, fields)=>{
+        if(!err){
+            var pageIndex = parseInt(request.params.pageIndex);
+            var total_page_length = Math.ceil(rows.length / 10);
+            var pagin_per_page = 5;
+            var start = (pageIndex - 1) * pagin_per_page;
+            var end = start + 10;
+            var pagin_start = pageIndex - 2;
+            var pagin_end = pageIndex + 3;
+
+            //edge cases for first few pages and last few pages
+            if(pagin_start < 1){
+                pagin_start = 1;
+                pagin_end = pagin_start + 5;
+            }
+            if(pagin_end > total_page_length){
+                pagin_end = total_page_length;
+                pagin_start = pagin_end - 5;
+            }
+            response.render('index', {all_posts: rows, start: start, end: end, pagin_start: pagin_start, pagin_end: pagin_end, last_page: total_page_length - 1, currPage: pageIndex});
+        }
+        else{
+            console.log(err);
+        }
+    })
+});
+
+app.get("/users", (req, res) => {
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    res.render('users', {users: users_array});
+})
+app.listen(8000, () => console.log("listening on port 8000"));
